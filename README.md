c1a687d51e6d11122d0e24391c9bb54ef34b50cb
Evgeny Blokhin <eb@tilde.pro>
2017-08-03 17:34:53 +0200

    Re-structuring and separating Python client, as suggested by Martin Uhrin

--- README.md
+++ README.md
@@ -1,8 +1,4 @@
-MPDS (Materials Platform for Data Science): API and introduction for developers
+MPDS platform API client written in Python
 ==========
 
-![MPDS: Materials Platform for Data Science](https://raw.githubusercontent.com/mpds-io/datascience/gh-pages/mpds.png "MPDS: Materials Platform for Data Science")
-
-This repository currently contains the issue tracker and the website [developer.mpds.io](http://developer.mpds.io) with the MPDS API documentation.
-
-All the information here is opened and freely available under the [MIT license](https://en.wikipedia.org/wiki/MIT_License). Please, contact us at <sales@tilde.pro>, if you'd like to use the MPDS API.
+Please, see [developer.mpds.io](http://developer.mpds.io).
---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+
+install_requires = [
+    'httplib2',
+    'ujson',
+    'jsonschema', # only needed for a test
+    'jmespath', # optional, can work without it
+    'pandas == 0.20.1', # optional, can work without it
+    'ase == 3.11' # can be pymatgen instead
+]
+
+setup(
+    name='mpds_client',
+    version='0.0.7',
+    author='Evgeny Blokhin',
+    author_email='eb@tilde.pro',
+    description='MPDS platform API client',
+    long_description='This Python library takes care of many aspects of the MPDS API, such as pagination, error handling, validation, proper data extraction and more. We encourage our users to adopt this library for their needs.',
+    url='https://github.com/mpds-io/python-api-client',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Chemistry',
+        'Topic :: Scientific/Engineering :: Physics',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ],
+    keywords='materials informatics PAULING FILE data MPDS platform API',
+    packages=['mpds_client'],
+    install_requires=install_requires
+)
