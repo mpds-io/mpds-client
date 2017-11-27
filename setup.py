bf9f24dfa7c9750a82e69a97ebaa2e4362c4a4a4
Evgeny Blokhin <eb@tilde.pro>
2017-11-27 17:52:07 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ install_requires = [
 
 setup(
     name='mpds_client',
-    version='0.0.12',
+    version='0.0.13',
     author='Evgeny Blokhin',
     author_email='eb@tilde.pro',
     description='MPDS platform API client',
